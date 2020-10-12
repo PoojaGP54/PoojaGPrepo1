036eacd59e450fadf7e3aacecee8c643be1d74e6
Pooja G.P <poojagp.entransys@gmail.com>
2020-10-12 17:13:24 +0530

    Initial commit

---
+++ src/main/java/com/hcl/poojaGPdemo1/PoojaGPdemo1Application.java
@@ -0,0 +1,13 @@
+package com.hcl.poojaGPdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoojaGPdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoojaGPdemo1Application.class, args);
+	}
+//this is where execution starts.
+}
---
+++ src/test/java/com/hcl/poojaGPdemo1/PoojaGPdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.poojaGPdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoojaGPdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
